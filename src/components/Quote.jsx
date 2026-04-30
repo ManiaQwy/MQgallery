b260f81ecd41a9638807687e32d8ea45293a9915
Bartosz Molendowski <bartekmol2007@gmail.com>
2026-04-30 10:31:32 +0200

    quotesize fix

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,6 +4,8 @@ const BlockQuote = styled.blockquote`
     font-size: 3rem;
     margin: 0 0 3rem 0;
     padding:0 2rem;
+    width: 100%;
+    max-width: 60rem;
 `
 export default function Quote({children}){
     return(
